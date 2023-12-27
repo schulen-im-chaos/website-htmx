52cd7b86ff856e713ffbf853cb11d78e55bdbf75
alexcoder04 <alexcoder04@protonmail.com>
2023-12-27 22:14:05 +0100

    some appearance changes

--- static/content/tailwind.config.js
+++ static/content/tailwind.config.js
@@ -6,17 +6,23 @@ module.exports = {
   },
   daisyui: {
     themes: [
+      "light",
       {
-        dark: {
-          ...require("daisyui/src/theming/themes")["dark"],
-          "base-content": "rgb(226 232 240)",
-          "neutral-content": "rgb(226 232 240)",
+        mytheme: {
+          "color-scheme": "dark",
+          primary: "oklch(65.69% 0.196 275.75)",
+          secondary: "oklch(74.8% 0.26 342.55)",
+          accent: "oklch(74.51% 0.167 183.61)",
+          neutral: "#2a323c",
+          "neutral-content": "#e8f0ff",
+          "base-100": "#1d232a",
+          "base-200": "#191e24",
+          "base-300": "#15191e",
+          "base-content": "#e8f0ff",
         },
       },
     ],
+    darkTheme: "mytheme",
   },
-  plugins: [
-    require("@tailwindcss/typography"),
-    require('daisyui'),
-  ]
-}
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+};
