5f7bef395fe983fbd587df4762e8490652b4114e
nwrenger <nils@wrenger.net>
2024-01-04 21:10:17 +0100

    :sparkles: Initial Commit

---
+++ src/routes/materials/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let data;
+</script>
+
+<h2 class="h2">Lernmaterialien</h2>
+<p>
+	Diese Lernmaterialien wurden von anderen Schüler*innen erstellt, um Inhalte auf eine verständliche
+	Art und Weise kurz zu erklären.
+</p>
+<p>Derzeit wirst Du hier noch nicht so viel finden und deshalb brauchen wir Deine Hilfe!</p>
+<p>
+	Hast Du eigene Lernzettel, die du mit anderen teilen willst? Dann geht's <a
+		class="anchor"
+		href="/materials/contribute"
+		data-sveltekit-preload-data="hover">hier lang</a
+	>!
+</p>
+<div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 gap-6">
+	{#each data.subjects as subject}
+		<a class="btn variant-filled" href="/materials/kind/{subject}">{subject}</a>
+	{/each}
+</div>
--- web/template/vars.go
+++
@@ -1,5 +0,0 @@
-package template
-
-const DiscordURL = "https://discord.com/invite/nWd8yZ4HWY"
-const InstagramURL = "https://www.instagram.com/schulenimchaos/"
-const GithubURL = "https://github.com/schulen-im-chaos"
